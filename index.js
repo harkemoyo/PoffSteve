32868ccfe45bda2f18fc9ae942c64a1538a243ef
harkemoyo <markndeche91@gmail.com>
2023-07-21 11:24:28 +0300

    added bind method

--- index.js
+++ index.js
@@ -1346,3 +1346,31 @@ box.addEventListener('mouseup', myFunc2);
 // myFunc.call(window);
 myFunc.apply(box);
 // myFunc2()
+
+// Apply call and bind methods
+
+let bob = function(num, str, x){
+  console.log('bob', num, str, this, x);
+  return true;
+}
+let bill = {
+  name: 'Bill Murray',
+  movie: 'Lost in Translation',
+  myMethod: function(fn){
+      //fn(2, 'hello');
+      let n = arguments[1];
+      let s = arguments[2];
+      fn.apply(bill, [n, s]);
+      //fn.call(bill, n, s);
+  }
+}
+
+let fred = bob.bind(bill, 5, 'hasta la vista');
+fred('x');
+
+//bob(1, 'hello');
+//bill.myMethod(bob);
+//bob.call(bill, 2, 'goodbye');
+//let arr = [3, 'hi'];
+//bob.apply(bill, arr);
+//bill.myMethod(bob, 4, 'ciao');
